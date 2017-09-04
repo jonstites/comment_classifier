5206cfa43dd6f8ffd289ee5808f4bb67323f0d39
Jonathan Stites <mail@jonstites.com>
2017-09-04 17:46:26 -0500

    use f1, not accuracy

--- bin/commenter.py
+++ bin/commenter.py
@@ -6,39 +6,40 @@ from comment_generator.core import MarkovModel, CommentReader
 import numpy
 
 
-def run(comment_file, test_file, ngram_size, num_subreddits):
-    print("performing on: {}".format(ngram_size))
-    feature_size = (2**30 / num_subreddits)
-    model = MarkovModel(ngram_size, feature_size)
 
-    subreddits_set = set()
-    train_total = 0
+
+def run(comment_file, test_file, subreddit_counts, ngram_size):
+
+    model = MarkovModel(ngram_size, subreddit_counts, max_size=2**30)
+
+    num = 0
     with bz2.BZ2File(comment_file, "rb") as input_handle:
-        for comment in CommentReader.from_open_file(input_handle):
-            subreddit = comment.get_subreddit()
-            subreddits_set.add(subreddit)
-            train_total += 1
-            
-    with bz2.BZ2File(test_file, "rb") as test_handle:
-        for comment in CommentReader.from_open_file(test_handle):
-            subreddit = comment.get_subreddit()
-            subreddits_set.add(subreddit)
+        with bz2.BZ2File(test_file, "rb") as test_handle:
+            while input_handle.peek() and test_handle.peek():
+                model.train(input_handle, subreddit_counts, num_batches=3, batch_size=50000)
+                f1 = model.f1_macro(test_handle, subreddit_counts, num_batches=1, batch_size=10000)
+                print("{} f1: {}".format(num, f1), flush=True)
+                num += 1
 
 
-    subreddits = numpy.array(list(subreddits_set))
-    print("classifying {} subreddits".format(len(subreddits)))
-    with bz2.BZ2File(comment_file, "rb") as input_handle:
-        print("total training comments: ", train_total)
-        model.train(input_handle, subreddits)
+def get_num_features(subreddits, max_size=2**30):
+    num_subreddits = len(subreddits.keys())
+    return int(max_size / num_subreddits)
 
-
-    with bz2.BZ2File(test_file, "rb") as test_handle:
-        model.score(test_handle, subreddits)
-
-
-def main(comment_file, test_file):
-    for ngram_size in range(1, 6):
-        run(comment_file, test_file, ngram_size)
+def get_subreddit_counts(subreddit_file, top):
+    subreddits = {}
+    with open(subreddit_file) as handle:
+        for line_num, line in enumerate(handle):
+            subreddit = line.split()[0]
+            count = int(line.split()[1])
+            if line_num >= top:
+                break
+            subreddits[subreddit] = count
+    return subreddits
+        
+def main(comment_file, test_file, subreddit_file, top=100, ngram_size=3):
+    subreddit_counts = get_subreddit_counts(subreddit_file, top)
+    run(comment_file, test_file, subreddit_counts, ngram_size)
         
 if __name__ == "__main__":
     argh.dispatch_command(main)
--- comment_generator/core.py
+++ comment_generator/core.py
@@ -2,8 +2,10 @@
 
 from collections import Counter
 import json
+import numpy
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import f1_score
 
 class Comment:
 
@@ -46,7 +48,17 @@ class CommentReader:
             comments.append(comment)
             if len(comments) == batch_size:
                 yield comments
+                comments = []
+        yield comments
 
+    def split_comments(comments):
+        texts = []
+        subreddits = []
+        for comment in comments:
+            texts.append(comment.get_body())
+            subreddits.append(comment.get_subreddit())
+        return texts, subreddits
+            
     def get_subreddit_counts(opened_handle):
         counts = Counter()
         for comment in CommentReader.from_open_file(opened_handle):
@@ -56,25 +68,51 @@ class CommentReader:
                 
 class MarkovModel:
 
-    def __init__(self, max_ngram, feature_num):
-        self.classifier = SGDClassifier(n_jobs=-1)
-        self.vectorizer = HashingVectorizer(n_features=feature_num, ngram_range=(1, max_ngram))
-        
-    def train(self, file_handle, use_subreddits, batch_size=1000000):
-        for number, batch_comments in enumerate(CommentReader.get_text_batch(file_handle, use_subreddits=use_subreddits, batch_size=batch_size)):
-            texts, subreddits = batch_comments
-            feature_matrix = self.vectorizer.transform(texts)
-            self.classifier.partial_fit(feature_matrix, subreddits, classes=classes)
-            print("completed batch {0}".format(number))
+    def __init__(self, max_ngram, subreddit_counts, max_size):
+        num_features = self.get_num_features(subreddit_counts, max_size)
+        self.vectorizer = HashingVectorizer(n_features=num_features, ngram_range=(1, max_ngram))
 
-    def score(self, file_handle, use_subreddits, batch_size=1000000):
-        for number, batch_comments in enumerate(CommentReader.get_text_batch(file_handle, use_subreddits=use_subreddits, batch_size=batch_size)):
-            texts, subreddits = batch_comments
+        self.classes = numpy.array(list(subreddit_counts.keys()))
+        class_weights = self.get_balanced_class_weights(subreddit_counts)
+        
+        self.classifier = SGDClassifier(n_jobs=-1, class_weight=class_weights)
+        
+    def train(self, file_handle, use_subreddits, batch_size=1000000, num_batches=None):
+        for batch_number, batch_comments in enumerate(CommentReader.get_batch(file_handle, use_subreddits=use_subreddits, batch_size=batch_size)):
+            texts, subreddits = CommentReader.split_comments(batch_comments)
             feature_matrix = self.vectorizer.transform(texts)
-            mean_accuracy = self.classifier.score(feature_matrix, subreddits)
-            print("batch {0} accuracy: {1}".format(number, mean_accuracy))
+
+            self.classifier.partial_fit(feature_matrix, subreddits, classes=self.classes)
+
+            if num_batches:
+                about_to_go_over = batch_number >= (num_batches - 1)
+                if about_to_go_over:
+                    return
+
+    def f1_macro(self, file_handle, use_subreddits, batch_size=1000000, num_batches=None):
+        for batch_number, batch_comments in enumerate(CommentReader.get_batch(file_handle, use_subreddits=use_subreddits, batch_size=batch_size)):
+            texts, subreddits = CommentReader.split_comments(batch_comments)
+            feature_matrix = self.vectorizer.transform(texts)
+
+            predicted_subreddits = self.classifier.predict(feature_matrix)
+
+            f1 = f1_score(subreddits, predicted_subreddits, average="macro")
+            return f1
             
+    def get_num_features(self, subreddit_counts, max_size=2**30):
+        num_subreddits = len(subreddit_counts.keys())
+        return int(max_size / num_subreddits)
+
+    def get_balanced_class_weights(self, subreddit_counts):
+        weights = {}
+        total = sum(subreddit_counts.values())
+        num_classes = len(subreddit_counts.keys())
+        
+        for subreddit, count in subreddit_counts.items():
+            weights[subreddit] = total / (num_classes * count)
             
+        return weights
+    
     def get_vocabulary(file_handle):
         pass
 
